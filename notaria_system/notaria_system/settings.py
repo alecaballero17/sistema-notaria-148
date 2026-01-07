afaf0d31f7c3ecd37df04190ef33734cc12627c3
alecaballero17 <alecaballeropereira@gmail.com>
2026-01-07 01:40:27 -0400

    Actualizar configuración de plantillas y crear base de HTML para la recepción de clientes

--- notaria_system/notaria_system/settings.py
+++ notaria_system/notaria_system/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'notaria_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'notaria_system' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
